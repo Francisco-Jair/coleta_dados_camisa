e378f2dacc4438304784a3c680cce9f8ff51a843
Francisco Jair <fjair123@gmail.com>
2022-11-18 09:27:03 -0300

    feat: add dependecias

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ dataset = pd.read_csv("captura_dados_camisa.csv")
 # url = dataset["link_para_publicacao"]
 # vai até o 3051
 
-cont = 41
+cont = 42
 for urls in dataset["link_para_publicacao"][cont:61]:
     coleta_camisa(urls)
     print("OK")
